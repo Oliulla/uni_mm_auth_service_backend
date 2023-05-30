ecd47be9f3757a6081dc084e7cbc902574478643
Oliulla <razasm29@gmail.com>
2023-05-30 10:55:42 +0600

    [done] setup express, mongoose, cors, purser

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import config from "./config/index";
+import app from "./app";
+
+
+async function bootstrap() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log(`Database connected successfully!`);
+
+    app.listen(config.port, () => {
+      console.log(`Univ Management app listening on port ${config.port}`)
+    })
+
+  } catch (error) {
+    console.log(`Failed to connect with DB!!!`);
+  }
+}
+
+bootstrap()
